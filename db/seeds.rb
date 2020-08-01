9c09bf1b4e1b95ed02574631a968145209a8fa75
mattdavidthomas <MatthewThomas568325@gmail.com>
2020-08-01 20:51:56 +1000

    added websocket with ActionCable, broadcasting, auto-scrolling and final UI changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Message.create(body: "random body of words blah blah blah", user_id: 1)
+Message.create(body: "alternative body of words blah blah blah", user_id: 2)
+Message.create(body: "apple body of words blah blah blah", user_id: 3)
+Message.create(body: "weird body of words blah blah blah", user_id: 1)
+Message.create(body: "body of words blah blah blah", user_id: 2)
